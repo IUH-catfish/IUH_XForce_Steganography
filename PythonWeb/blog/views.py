f5a05173ce09a664d1a2ea30de9160666e9a88c3
IUH_catfish <dangtuanminh7399@gmail.com>
2024-10-01 15:21:34 +0700

    Adding Bootstrap

--- PythonWeb/blog/views.py
+++ PythonWeb/blog/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+posts = [
+    {
+        'author' : 'Me',
+        'title' : "About my block",
+        'content' : 'First Post',
+        'date_posted' : 'August 28, 2018',
+    },
+    {
+        'author' : 'Me Again',
+        'title' : "About my block num 2",
+        'content' : 'Second Post',
+        'date_posted' : 'August 29, 2018',
+    }
+] 
+
 def home(request):
+    context = {
+        'posts' : posts
+    }
+    
     respond = '<h1>Home Blog Here</h1>'
-    return HttpResponse(respond)
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    respond = '<h1>About This Blog Here</h1>'
+    return render(request, 'blog/about.html')
+
